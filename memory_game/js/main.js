25660e8fb895092b74e3eae55c565a661d89f2c4
Emily Colon <EC86638@ad1.prod>
2018-05-03 11:55:27 -0400

    corrected game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,14 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var checkForMatch = function () {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!");
+        } else {
+            alert("Sorry, try again.");
+        }
     } else {
-        alert("Sorry, try again.");
+        return;
     }
 }
 var flipCard = function (cardId) {
